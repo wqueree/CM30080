9c836ffd13403792c080ff2d2e643d3d3a5dba1e
Will Queree <wqueree@gmail.com>
2023-03-14 12:14:34 +0000

    Writes output images with labels

--- task2/bounding_box.py
+++ task2/bounding_box.py
@@ -1,27 +1,39 @@
 import cv2
 import numpy as np
 
+from colors import RED
 from pathlib import Path
 from typing import List, Union, Tuple
 
 
-def get_bounding_boxes_file(image_path: Union[Path, str], min_contour_area: int = 1500, max_contour_area: int = 100000) -> List[Tuple[int, float]]:
+def get_bounding_boxes_file(image_path: Union[Path, str], min_contour_area: int, max_contour_area: int) -> List[Tuple[int, float]]:
     image: np.ndarray = cv2.imread(str(image_path))
     return get_bounding_boxes(image, min_contour_area, max_contour_area)
 
 
-def get_bounding_boxes(image: np.ndarray, min_contour_area: int = 1500, max_contour_area: int = 100000) -> List[Tuple[int, float]]:
+def get_bounding_boxes(image: np.ndarray, min_contour_area: int, max_contour_area: int) -> List[Tuple[int, float]]:
     bounding_boxes: List[Tuple[int, float]] = list()
     image_greyscale: np.ndarray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     _, image_binary = cv2.threshold(image_greyscale, 240, 255, cv2.THRESH_BINARY)
     contours, _ = cv2.findContours(image_binary, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-    for contour in contours:
+    contour_areas: List[float] = list()
+    for i, contour in enumerate(contours):
         contour_area: float = cv2.contourArea(contour)
         if min_contour_area < contour_area < max_contour_area:
             bounding_boxes.append(cv2.boundingRect(contour))
-    return bounding_boxes
+            contour_areas.append(contour_area)
+    sorted_bounding_boxes: List[Tuple[int, float]] = [bounding_box for _, bounding_box in sorted(zip(contour_areas, bounding_boxes))]
+    return sorted_bounding_boxes
 
 
-def render_bounding_boxes(image: np.ndarray, bounding_boxes: List[Tuple[int, float]], color: Tuple[int] = (0, 0, 255)) -> None:
-    for x, y, w, h in bounding_boxes:
-        cv2.rectangle(image, (x, y), (x + w, y + h), (0, 0, 255), 1)
+def render_bounding_boxes(image: np.ndarray, bounding_boxes: List[Tuple[int, float]], color: Tuple[int] = RED) -> None:
+    for bounding_box in bounding_boxes:
+        render_bounding_box(image, bounding_box)
+
+
+def render_bounding_box(image: np.ndarray, bounding_box: Tuple[int, float], color: Tuple[int] = RED, label_top: str = "", label_bottom: str = "") -> None:
+    x, y, w, h = bounding_box
+    cv2.rectangle(image, (x, y), (x + w, y + h), RED, 1)
+    cv2.putText(image, label_top, (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, RED, 1)
+    cv2.putText(image, label_bottom, (x, y + h + 15), cv2.FONT_HERSHEY_SIMPLEX, 0.5, RED, 1)
+
--- task2/task2.py
+++ task2/task2.py
@@ -1,36 +1,59 @@
 import cv2
 import numpy as np
 
-from bounding_box import get_bounding_boxes, get_bounding_boxes_file, render_bounding_boxes
-from image_compare import compare_rgb, compare_rgb_file, compare_grayscale, compare_grayscale_file
+from bounding_box import get_bounding_boxes, render_bounding_box
+from colors import WHITE
+from image_compare import compare_rgb
 from pathlib import Path
 from typing import Dict, List, Tuple
 
 
 def main() -> None:
+    predict_directory_path: Path = Path("./predict")
+    predict_directory_path.mkdir(parents=True, exist_ok=True)
     test_directory_path: Path = Path("./test/images").resolve(strict=True)
     train_directory_path: Path = Path("./train/png").resolve(strict=True)
-    
+    icons: Dict[str, np.ndarray] = get_train_icons(train_directory_path)
     for image_path in test_directory_path.glob("*.png"):
         image: np.ndarray = cv2.imread(str(image_path))
-        predict_icon_classes(image, train_directory_path)
+        predict_icon_classes(image, image_path.name, icons, predict_directory_path)
 
 
-def predict_icon_classes(image: np.ndarray, train_directory_path: Path) -> str:
-    bounding_boxes: List[Tuple[int, float]] = get_bounding_boxes(image)
-    for bounding_box in bounding_boxes:
-        # TODO Crop images using bboxes
+def predict_icon_classes(image: np.ndarray, image_name: str, icons: Dict[str, np.ndarray], predict_directory_path: Path) -> str:
+    bounding_boxes: List[Tuple[int, float]] = get_bounding_boxes(image, min_contour_area=1500, max_contour_area=100000)
+    image_icons: List[np.ndarray] = get_image_icons(image, bounding_boxes)
+    image_predict: np.ndarray = np.copy(image)
+    for bounding_box, image_icon in zip(bounding_boxes, image_icons):
         icon_errors: Dict[str, float] = dict()
-        for icon_path in train_directory_path.glob("*.png"):
-            icon = cv2.imread(str(icon_path))
-            # TODO Mask away background
-            # TODO Do comparison
+        for class_name, icon in icons.items():
+            scaled_icon: np.ndarray = cv2.resize(icon, image_icon.shape[1::-1], interpolation=cv2.INTER_AREA)
+            icon_errors[class_name] = compare_rgb(scaled_icon, image_icon)
+        prediction: str = min(icon_errors, key=icon_errors.get)
+        label_top, label_bottom = prediction.split("-", maxsplit=1)
+        render_bounding_box(image_predict, bounding_box, label_top=label_top, label_bottom=label_bottom)  
+    cv2.imwrite(f"{predict_directory_path}/{image_name}", image_predict)
 
-    # cv2.imshow("image", image)
-    # render_bounding_boxes(image, bounding_boxes)
-    # cv2.imshow("bounding_boxes", image)
-    # cv2.waitKey(0)
-    # cv2.destroyAllWindows()
+
+def get_train_icons(train_directory_path: Path) -> Dict[str, np.ndarray]:
+    cropped_icons: Dict[str, np.ndarray] = dict()
+    for icon_path in train_directory_path.glob("*.png"):
+        # Read icon and pad with 1px white border
+        icon: np.ndarray = cv2.imread(str(icon_path))
+        icon = cv2.copyMakeBorder(icon, 1, 1, 1, 1, cv2.BORDER_CONSTANT, None, WHITE)
+        # Crop icons
+        x, y, w, h = get_bounding_boxes(icon, min_contour_area=100000, max_contour_area=275000)[0]
+        icon_crop: np.ndarray = icon[y:y + h, x:x + w]
+        cropped_icons[icon_path.stem] = icon_crop
+    return cropped_icons
+
+
+def get_image_icons(image: np.ndarray, bounding_boxes: List[Tuple[int, float]]):
+    cropped_icons: Dict[str, np.ndarray] = list()
+    for x, y, w, h in bounding_boxes:
+        icon_crop: np.ndarray = image[y:y + h, x:x + w]
+        cropped_icons.append(icon_crop)
+    return cropped_icons
+
 
 if __name__ == "__main__":
     main()
